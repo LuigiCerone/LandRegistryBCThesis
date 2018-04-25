811e48663b451cdefd32b9c7c6a3eed6482a9028
luigi.cerone <luigiceroneaq@gmail.com>
2018-04-25 16:21:32 +0200

    App structure added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,22 +3,3 @@ var Migrations = artifacts.require("./Migrations.sol");
 module.exports = function (deployer) {
 	deployer.deploy(Migrations);
 };
-
-
-api.post('/progress', (req, res) => {
-	let progress = new Progress();
-	progress.user_id = req.body.user_id;
-	progress.level = req.body.level;
-	progress.iconName = req.body.iconName;
-	progress.category_id = req.body.category_id;
-
-	progress.save(function (err) {
-		if (err) {
-			res.send(err);
-		}
-		res.json({
-			message: 'progress saved!'
-		});
-
-	});
-});
