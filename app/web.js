d7454490152317b9efa91cd63be843155808c8b0
luigi.cerone <luigiceroneaq@gmail.com>
2018-04-26 11:31:14 +0200

    Web3 import syntax changed

--- app/web.js
+++ app/web.js
@@ -1,13 +1,8 @@
-var Web3 = require('web3');
+import Web3 from 'web3';
+const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 
-let web3 = window.web3;
+// Example to test if it is working.
+web3.eth.getAccounts().then(console.log);
 
-if (typeof web3 !== 'undefined') {
-	web3 = new Web3(web3.currentProvider)
-	console.log('Injected web3 detected!');	
-} else {
-	// set the provider you want from Web3.providers.
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-	console.log('No web3 instance injected, using Local web3.');
-}
-exports.web3 = web3;
+// Export default because it is the only thing exported.
+export default web3;
