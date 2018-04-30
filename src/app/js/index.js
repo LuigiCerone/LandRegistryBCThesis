dd01463149262cdd4a621b6203ee035f59c8e496
luigi.cerone <luigiceroneaq@gmail.com>
2018-04-30 10:28:34 +0200

    Default account settings added

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,8 +1,22 @@
 import web3 from './web3';
 
 // web3.eth.getAccounts().then(console.log);
-import unity_abi from  'CompiledContracts/Unity.json'; 
-// Contract address is taken by Truffle when migrations are executed and is used to 
-// identify a specific smart contract.
-var UnityContractAddress = "0x5c7eef2cbaaa5ba17d6b7f973e01ce6d46d2cf20";
-var UnityContract = new web3.eth.Contract(unity_abi.abi, UnityContractAddress);
+import unity_abi from 'CompiledContracts/Unity.json';
+
+// Setup of defaultAccount.
+web3.eth.getAccounts().then((accounts) => {
+	web3.eth.defaultAccount = accounts[0];
+	start();
+}).catch((err) => console.log("Error while setting the default account!"));
+
+
+function start() {
+	console.log(web3.eth.defaultAccount);
+	// Contract address is taken by Truffle when migrations are executed and is used to 
+	// identify a specific smart contract.
+	var UnityContractAddress = "0x5c7eef2cbaaa5ba17d6b7f973e01ce6d46d2cf20";
+
+	// Instantiate UnityContract.
+	var UnityContract = new web3.eth.Contract(unity_abi.abi, UnityContractAddress);
+	console.log(UnityContract.methods);
+}
