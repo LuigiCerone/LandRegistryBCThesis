ff5b0bde31c7a6a759409cbcbc04077e876b66f1
luigi.cerone <luigiceroneaq@gmail.com>
2018-05-01 12:11:02 +0200

    Testing truffle-contract

--- app/js/index.js
+++ app/js/index.js
@@ -1,28 +1,49 @@
 import web3 from './web3';
 import $ from 'jquery';
+import TruffleContract from 'truffle-contract'
 
 // web3.eth.getAccounts().then(console.log);
 import unity_abi from 'CompiledContracts/Unity.json';
 // Setup of defaultAccount.
 
 web3.eth.getAccounts().then((accounts) => {
-	console.log("qui");
 	web3.eth.defaultAccount = accounts[0];
-	console.log(accounts);
-}, function (error) {
-	console.log(error)
-});
-console.log("qua");
+	start();
+}).catch((err) => console.log(err));
 
-// function start() {
-// 	console.log(web3.eth.defaultAccount);
-// 	// Contract address is taken by Truffle when migrations are executed and is used to 
-// 	// identify a specific smart contract.
-// 	// var UnityContractAddress = "0x888af8dd6a24cc46b1ae83872d0b4037a7cbb7e2";
+function start() {
+	// console.log(web3.eth.defaultAccount);
+	// Contract address is taken by Truffle when migrations are executed and is used to 
+	// identify a specific smart contract.
+	// var UnityContractAddress = "0x888af8dd6a24cc46b1ae83872d0b4037a7cbb7e2";
 
-// 	// Instantiate UnityContract.
-// 	var UnityContract = new web3.eth.Contract(unity_abi.abi);
-// 	var contractInstance;
+	// Instantiate UnityContract.
+	var UnityContract = new web3.eth.Contract(unity_abi.abi);
+	console.log(UnityContract);
+	var contractInstance = UnityContract.deploy({
+		data: unity_abi.bytecode,
+		arguments: [0, (web3.utils.asciiToHex('324'))]
+	});
+	console.log(contractInstance);
+
+	console.log("Other test: --------------------------------");
+
+	var UnityContract2 = TruffleContract(unity_abi);
+	UnityContract2.setProvider(web3.currentProvider);
+	// if (typeof UnityContract2.currentProvider.sendAsync !== "function") {
+	// 	UnityContract2.currentProvider.sendAsync = function() {
+	// 	  return UnityContract2.currentProvider.send.apply(
+	// 		UnityContract2.currentProvider, arguments
+	// 	  );
+	// 	};
+	//   }
+	var contractInstance2;
+	UnityContract2.deployed().then((instance) => contractInstance2 = instance);
+	console.log("Istance2: " + contractInstance2);
+
+}
+
+
 // 	console.log(UnityContract);
 
 // 	// UnityContract.methods.getOwner().call().then(console.log).catch(console.log);
--- app/js/web3.js
+++ app/js/web3.js
@@ -2,9 +2,8 @@ const Web3 = require('web3');
 // const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-
 // Example to test if it is working.
-web3.eth.getAccounts().then(console.log);
-console.log(web3);
+// web3.eth.getAccounts().then(console.log);
+
 // Export default because it is the only thing exported.
 export default web3;
