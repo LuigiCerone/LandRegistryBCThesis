1f86a2b9eaf0378f94322658b4df20647008e795
luigi.cerone <luigiceroneaq@gmail.com>
2018-07-05 18:36:40 +0200

    Added logger, bodyparser, dotenv

--- server/main.js
+++ server/main.js
@@ -1,35 +1,56 @@
 'use strict';
 const express = require('express');
+const body_parser = require('body-parser');
+const morgan = require('morgan');
+const dotenv = require('dotenv').config();
+
 const logger = require('./logger');
+const routes = require('./routes');
 
 const app = express();
-const PORT = 3000;
 
 
 module.exports = {
     start({
               env = process.env.NODE_ENV || 'development'
           }) {
+        logger.info(`Running mode ${env}`);
         //Serve static assets (js, css, ...)
-        app.use(express.static('app'));
+        // app.use(express.static('app'));
 
         //Serve contract artifact files (ex: ToDo.json)
         // app.use(express.static('build/contracts'));
 
-        //Serve index.html
-        app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/app/index.html');
-        });
+        // Set application port.
+        app.set('port', process.env.PORT || 3000);
+
+        // Set routes.
+        app.use('/rest/v1/', routes);
+
+        // Setup body parser.
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+
+        // Setup logger for express HTTP requests.
+        app.use(morgan('dev'));
+
+        // //Serve index.html
+        // app.get('/', (req, res) => {
+        //     res.sendFile(__dirname + '/app/index.html');
+        // });
 
         //If anything else is requested that's an error
         app.get('*', (req, res) => {
+            logger.error("404 - Not found: " + req);
             res.status(404);
             res.send('Oops... this URL does not exist');
         });
 
 
         //Start the server
-        app.listen(PORT, () => {
+        app.listen(app.get('port'), () => {
             logger.info(`Ethereum app running on port ${PORT}...`);
         });
     }
---
+++ server/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+// Add controller requirments here;
+
+const router = express.Router();
+
+// Map each endpoint to router method.
+// router.post('/add', controller.method);
+
+module.exports = router;
